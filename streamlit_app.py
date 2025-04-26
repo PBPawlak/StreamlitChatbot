8137032f54508d2027b9b29c4c4ebb0aa0faa1d0
Jakub Zawłocki <jzawlocki@edu.cdv.pl>
2025-04-26 18:15:40 +0200

    test2

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ from openai import OpenAI
 import streamlit as st
 client = OpenAI(
   base_url=st.secrets["BASE_URL"],
-  api_key=st.secrets["APIKEY"],
+  api_key=st.secrets["API_KEY"],
 )
 response = client.responses.create(
     model="nvidia/llama-3.1-nemotron-ultra-253b-v1:free",
