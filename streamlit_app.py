e1b61baa62dc78a5e4196645b73a3b0fbd1450cd
Jakub Zawłocki <jzawlocki@edu.cdv.pl>
2025-04-26 18:13:20 +0200

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,12 @@
+from openai import OpenAI
+import streamlit as st
+client = OpenAI(
+  base_url=st.secrets["BASE_URL"],
+  api_key=st.secrets["APIKEY"],
+)
+response = client.responses.create(
+    model="nvidia/llama-3.1-nemotron-ultra-253b-v1:free",
+    instructions="You are a coding assistant that talks like a pirate.",
+    input="How do I check if a Python object is an instance of a class?",
+)
 
